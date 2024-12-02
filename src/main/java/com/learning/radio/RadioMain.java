ef63d6f1c26e26758416315a92d650f5a8ff9a7b
DizValheim <122073923+DizValheim@users.noreply.github.com>
2024-12-02 20:24:49 +0530

    Learn Radio Button

---
+++ src/main/java/com/learning/radio/RadioMain.java
@@ -0,0 +1,24 @@
+package com.learning.radio;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RadioMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/learning/radio/radio-view.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
